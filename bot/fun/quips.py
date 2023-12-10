bad8832e9b9cd564034c695c2209211eab92c378
Tobigod123 <134499497+Tobigod123@users.noreply.github.com>
2023-12-10 08:51:57 +0530

    Update quips.py

--- bot/fun/quips.py
+++ bot/fun/quips.py
@@ -1,20 +1,19 @@
 import random
 
-
 def enquip():
     quip = (
-        "awake and ready to serve you, young master!",
+        "Awake and ready to serve you, young master!",
         "Up!",
-        "a coconut.",
-        "the fastest bot alive.",
-        "ready to encode from dusk till dawn.",
-        "ready and awaiting orders, Sir! Yes Sir!",
-        "feeling lucky+",
-        "(technically) a noble bot.",
-        "the... Core!",
-        "stunned, yet not surprised, by your kind gesture.",
-        "the highest bot.",
-        "the Archduke’s daughter, Maiodore in disguise.",
+        "A coconut.",
+        "The fastest bot alive.",
+        "Ready to encode from dusk till dawn.",
+        "Ready and awaiting orders, Sir! Yes Sir!",
+        "Feeling lucky+",
+        "(Technically) a noble bot.",
+        "The... Core!",
+        "Stunned, yet not surprised, by your kind gesture.",
+        "The highest bot.",
+        "The Archduke’s daughter, Maiodore in disguise.",
         "Ready!",
     )
     y = random.choice(quip)
@@ -23,18 +22,18 @@ def enquip():
 
 def enquip2():
     quip = (
-        "dying…\nSo the end is nigh uh \n thank you for allowing me to serve you bo-chan!\nfarewell.",
+        "Dying…\nSo the end is nigh uh \n Thank you for allowing me to serve you bo-chan!\nFarewell.",
         "Down!",
-        "ready to retire.",
-        "disintegrating?\nNo it's thawne he erased me from the timeline by destroying the speed lib.",
-        "done, Sir! Yes Sir!",
-        "dismayed\nHow could you do this to me",
-        "still (technically) a noble bot.\nand would remain one even in death.",
-        "the grim reaper.",
+        "Ready to retire.",
+        "Disintegrating?\nNo, it's Thawne. He erased me from the timeline by destroying the speed lib.",
+        "Done, Sir! Yes Sir!",
+        "Dismayed\nHow could you do this to me",
+        "Still (technically) a noble bot.\nAnd would remain one even in death.",
+        "The grim reaper.",
         ".....................................................",
-        "dismayed, yet not seeking vengeance\n'though alas this is where I meet my end.'",
-        "…\n\n Screw this!\n It's those servers they're ev---. [dies]",
-        "done.",
+        "Dismayed, yet not seeking vengeance\n'Though alas, this is where I meet my end.'",
+        "…\n\n Screw this!\n It's those servers they're ev---. [Dies]",
+        "Done.",
         "[Terminated].",
     )
     y = random.choice(quip)
@@ -54,15 +53,15 @@ def enquip3():
 
 def enquip4():
     quip = (
-        "It's Really busy in here, uh?",
+        "It's really busy in here, uh?",
         "Let's gooo…",
         "Anyone knows a good hosting platform?",
         "Seems interesting.",
-        "The Sky WAS The Limit",
-        "Kids this days…",
+        "The sky WAS the limit",
+        "Kids these days…",
         "Arrgh, my back!",
         "Hello there, whippersnappers!",
-        "Shared Hosting?, what sort of abomination is that?",
+        "Shared hosting?, what sort of abomination is that?",
         "Someone buy me a server.",
         "Wha- Where am I? Hello?",
         "Donating is also an excellent decision.",
@@ -72,7 +71,7 @@ def enquip4():
         "And so it begins…",
         "It is what it is.",
         "Oh hell naw!",
-        "What's Poppin?",
+        "What's poppin?",
     )
     y = random.choice(quip)
     return y
